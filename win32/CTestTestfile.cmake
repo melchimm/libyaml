cdca6a35db63080fa974809ca225cb32851f108c
Melchi M. Michel <melchi.michel@rutgers.edu>
2017-09-07 10:42:40 -0400

    added x86 and x64 projects for buildng on Windows (MSVC 2010)

---
+++ win32/CTestTestfile.cmake
@@ -0,0 +1,28 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml
+# Build directory: C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+if("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(version "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/Debug/test-version.exe")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(version "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/Release/test-version.exe")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(version "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/MinSizeRel/test-version.exe")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(version "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/RelWithDebInfo/test-version.exe")
+else()
+  add_test(version NOT_AVAILABLE)
+endif()
+if("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(reader "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/Debug/test-reader.exe")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(reader "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/Release/test-reader.exe")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(reader "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/MinSizeRel/test-reader.exe")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(reader "C:/Users/melchi/Documents/Visual Studio 2010/LibraryProjects/libyaml/win32/RelWithDebInfo/test-reader.exe")
+else()
+  add_test(reader NOT_AVAILABLE)
+endif()
